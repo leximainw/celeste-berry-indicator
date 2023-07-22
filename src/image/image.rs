e44dfb509da931f174f839583aa3da1519680299
leximainw <leximainw@gmail.com>
2023-07-22 00:02:29 -0400

    feat: add  and  conversions

--- src/image/image.rs
+++ src/image/image.rs
@@ -9,3 +9,18 @@ pub struct Color {
     b: u32,
     a: u32,
 }
+
+impl Color {
+    pub fn from_rgba32(color: u32) -> Color {
+        Color {
+            r: (color >> 24 & 255) * 0x01010101,
+            g: (color >> 16 & 255) * 0x01010101,
+            b: (color >> 8 & 255) * 0x01010101,
+            a: (color & 255) * 0x01010101,
+        }
+    }
+
+    pub fn to_rgba32(color: Color) -> u32 {
+        (color.r >> 24 << 24) | (color.g >> 24 << 16) | (color.b >> 24 << 8) | (color.a >> 24)
+    }
+}
