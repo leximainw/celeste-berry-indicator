74a6b810ec6285aa104c7d58ad0c45fbd7055ea6
leximainw <leximainw@gmail.com>
2023-07-22 00:02:59 -0400

    feat: implement basic logic for RGBA32Image

--- src/image/rgba32image.rs
+++ src/image/rgba32image.rs
@@ -4,15 +4,17 @@ use super::{
 };
 
 pub struct RGBA32Image {
-
+    width: usize,
+    height: usize,
+    pixels: [u32],
 }
 
 impl Image for RGBA32Image {
     fn get_pixel(&self, pixel: (usize, usize)) -> Color {
-        todo!();
+        Color::from_rgba32(self.pixels[pixel.0 + pixel.1 * self.width])
     }
 
     fn set_pixel(&mut self, pixel: (usize, usize), color: Color) {
-        todo!();
+        self.pixels[pixel.0 + pixel.1 * self.width] = Color::to_rgba32(color);
     }
 }
