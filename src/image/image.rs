260cdee85a6a8efb4af6d16b0d0a263eda101a0a
leximainw <leximainw@gmail.com>
2023-07-22 00:01:08 -0400

    fix: change types for Image and Color fields to more appropriate types

--- src/image/image.rs
+++ src/image/image.rs
@@ -1,11 +1,11 @@
 pub trait Image {
-    fn get_pixel(&self, pixel: (i32, i32)) -> Color;
-    fn set_pixel(&mut self, pixel: (i32, i32), color: Color);
+    fn get_pixel(&self, pixel: (usize, usize)) -> Color;
+    fn set_pixel(&mut self, pixel: (usize, usize), color: Color);
 }
 
 pub struct Color {
-    r: i32,
-    g: i32,
-    b: i32,
-    a: i32,
+    r: u32,
+    g: u32,
+    b: u32,
+    a: u32,
 }
--- src/image/rgba32image.rs
+++ src/image/rgba32image.rs
@@ -8,11 +8,11 @@ pub struct RGBA32Image {
 }
 
 impl Image for RGBA32Image {
-    fn get_pixel(&self, pixel: (i32, i32)) -> Color {
+    fn get_pixel(&self, pixel: (usize, usize)) -> Color {
         todo!();
     }
 
-    fn set_pixel(&mut self, pixel: (i32, i32), color: Color) {
+    fn set_pixel(&mut self, pixel: (usize, usize), color: Color) {
         todo!();
     }
 }
