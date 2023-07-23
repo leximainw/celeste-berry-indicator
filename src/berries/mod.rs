617b28d42be2b37ca189f7baea7d1d04f0b3a756
leximainw <leximainw@gmail.com>
2023-07-23 12:37:23 -0400

    fix: add update to mod.rs

--- src/berries/mod.rs
+++ src/berries/mod.rs
@@ -1,6 +1,7 @@
 mod drawing;
 mod golden;
 mod golden_winged;
+mod moon;
 
 pub use drawing::Canvas as Canvas;
 pub use drawing::Drawable as Drawable;
@@ -8,3 +9,4 @@ pub use drawing::FadedCanvas as FadedCanvas;
 pub use drawing::OpaqueCanvas as OpaqueCanvas;
 pub use golden::GoldenBerry as GoldBerry;
 pub use golden_winged::WingedGoldenBerry as WingedGoldBerry;
+pub use moon::MoonBerry as MoonBerry;
