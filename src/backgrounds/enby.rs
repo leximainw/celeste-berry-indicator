5a812eca104aa26e593de562d3b9645096473cac
leximainw <leximainw@gmail.com>
2023-09-02 03:26:44 -0400

    feat: add nonbinary flag

---
+++ src/backgrounds/enby.rs
@@ -0,0 +1,31 @@
+use super::{
+    Color,
+    FlagStripes,
+    Generator,
+    Metagenerator,
+};
+
+pub struct FlagGenerator;
+
+impl Metagenerator for FlagGenerator {
+    fn get_generator(size: (usize, usize)) -> Box<dyn Generator> {
+        Box::new(Flag{
+            width: size.0,
+            height: size.1,
+        })
+    }
+}
+
+pub struct Flag {
+    width: usize,
+    height: usize,
+}
+
+impl Generator for Flag {
+    fn get_pixel(&self, pixel: (usize, usize)) -> Color {
+        const STRIPES: &'static [(u32, usize)] = &[
+            (0xfcf431ff, 1), (0xfcfcfcff, 1), (0x9d59d2ff, 1), (0x282828ff, 1),
+        ];
+        FlagStripes::stripes(STRIPES, pixel.1, self.height)
+    }
+}
--- src/backgrounds/mod.rs
+++ src/backgrounds/mod.rs
@@ -7,6 +7,10 @@ pub use generator::Generator as Generator;
 mod flag_stripes;
 pub use flag_stripes::FlagStripes as FlagStripes;
 
+mod enby;
+pub use enby::FlagGenerator as EnbyFlagGen;
+pub use enby::Flag as EnbyFlag;
+
 mod trans;
 pub use trans::FlagGenerator as TransFlagGen;
 pub use trans::Flag as TransFlag;
