899edcdfff9c4025fab2b9a2210d2f8a4c37a739
leximainw <leximainw@gmail.com>
2023-09-02 04:26:26 -0400

    fix: remove stripe weights (unsupported feature)

--- src/backgrounds/mod.rs
+++ src/backgrounds/mod.rs
@@ -27,7 +27,7 @@ macro_rules! flag_horiz {
 
         impl Generator for $flag {
             fn get_pixel(&self, pixel: (usize, usize)) -> Color {
-                const STRIPES: &'static [(u32, usize)] = $stripes;
+                const STRIPES: &'static [u32] = $stripes;
                 FlagStripes::stripes(STRIPES, pixel.1, self.height)
             }
         }
@@ -35,45 +35,45 @@ macro_rules! flag_horiz {
 }
 
 flag_horiz!(AroaceFlagGen, AroaceFlag, &[
-    (0xe28c00ff, 1), (0xeccd00ff, 1), (0xffffffff, 1), (0x62aedcff, 1), (0x203856ff, 1),
+    0xe28c00ff, 0xeccd00ff, 0xffffffff, 0x62aedcff, 0x203856ff,
 ]);
 
 flag_horiz!(AromanticFlagGen, AromanticFlag, &[
-    (0x3ba740ff, 1), (0xa8d47aff, 1), (0xffffffff, 1), (0xabababff, 1), (0x000000ff, 1),
+    0x3ba740ff, 0xa8d47aff, 0xffffffff, 0xabababff, 0x000000ff,
 ]);
 
 flag_horiz!(AsexualFlagGen, AsexualFlag, &[
-    (0x000000ff, 1), (0xa4a4a4ff, 1), (0xffffffff, 1), (0x810081ff, 1),
+    0x000000ff, 0xa4a4a4ff, 0xffffffff, 0x810081ff,
 ]);
 
 flag_horiz!(BisexualFlagGen, BisexualFlag, &[
-    (0xd60270ff, 2), (0x9b4f96ff, 1), (0x0038a8ff, 2),
+    0xd60270ff, 0xd60270ff, 0x9b4f96ff, 0x0038a8ff, 0x0038a8ff,
 ]);
 
 flag_horiz!(EnbyFlagGen, EnbyFlag, &[
-    (0xfcf431ff, 1), (0xfcfcfcff, 1), (0x9d59d2ff, 1), (0x282828ff, 1),
+    0xfcf431ff, 0xfcfcfcff, 0x9d59d2ff, 0x282828ff,
 ]);
 
 flag_horiz!(GayFlagGen, GayFlag, &[
-    (0x018e71ff, 1), (0x21cfabff, 1), (0x99e9c2ff, 1), (0xffffffff, 1), (0x7cafe3ff, 1), (0x4f47cdff, 1), (0x3b1379ff, 1),
+    0x018e71ff, 0x21cfabff, 0x99e9c2ff, 0xffffffff, 0x7cafe3ff, 0x4f47cdff, 0x3b1379ff,
 ]);
 
 flag_horiz!(LesbianFlagGen, LesbianFlag, &[
-    (0xd62800ff, 1), (0xf07527ff, 1), (0xff9b56ff, 1), (0xffffffff, 1), (0xd462a6ff, 1), (0xb55592ff, 1), (0xa40062ff, 1),
+    0xd62800ff, 0xf07527ff, 0xff9b56ff, 0xffffffff, 0xd462a6ff, 0xb55592ff, 0xa40062ff,
 ]);
 
 flag_horiz!(Lesbian5FlagGen, Lesbian5Flag, &[
-    (0xd62800ff, 1), (0xff9b56ff, 1), (0xffffffff, 1), (0xd462a6ff, 1), (0xa40062ff, 1),
+    0xd62800ff, 0xff9b56ff, 0xffffffff, 0xd462a6ff, 0xa40062ff,
 ]);
 
 flag_horiz!(QueerFlagGen, QueerFlag, &[
-    (0x000000ff, 1), (0x9ad9eaff, 1), (0x00a3e8ff, 1), (0xb5e51dff, 1), (0xffffffff, 1), (0xffc90dff, 1), (0xfc6667ff, 1), (0xfeaec9ff, 1), (0x000000ff, 1),
+    0x000000ff, 0x9ad9eaff, 0x00a3e8ff, 0xb5e51dff, 0xffffffff, 0xffc90dff, 0xfc6667ff, 0xfeaec9ff, 0x000000ff,
 ]);
 
 flag_horiz!(RainbowFlagGen, RainbowFlag, &[
-    (0xe50000ff, 1), (0xff8d00ff, 1), (0xffee00ff, 1), (0x028121ff, 1), (0x004cffff, 1), (0x770088ff, 1),
+    0xe50000ff, 0xff8d00ff, 0xffee00ff, 0x028121ff, 0x004cffff, 0x770088ff,
 ]);
 
 flag_horiz!(TransFlagGen, TransFlag, &[
-    (0x5bcefaff, 1), (0xf5a9b8ff, 1), (0xffffffff, 1), (0xf5a9b8ff, 1), (0x5bcefaff, 1),
+    0x5bcefaff, 0xf5a9b8ff, 0xffffffff, 0xf5a9b8ff, 0x5bcefaff,
 ]);
