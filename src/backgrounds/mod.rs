c3ef6866dadb4a4f648bd4711ef911a6fc4cdfd9
leximainw <leximainw@gmail.com>
2023-09-02 03:54:22 -0400

    refactor: consolidate flag boilerplate into a macro

--- src/backgrounds/mod.rs
+++ src/backgrounds/mod.rs
@@ -7,10 +7,37 @@ pub use generator::Generator as Generator;
 mod flag_stripes;
 pub use flag_stripes::FlagStripes as FlagStripes;
 
-mod enby;
-pub use enby::FlagGenerator as EnbyFlagGen;
-pub use enby::Flag as EnbyFlag;
+macro_rules! flag_horiz {
+    ($gen:ident, $flag:ident, $stripes:expr) => {
+        pub struct $gen;
 
-mod trans;
-pub use trans::FlagGenerator as TransFlagGen;
-pub use trans::Flag as TransFlag;
+        impl Metagenerator for $gen {
+            fn get_generator(&self, size: (usize, usize)) -> Box<dyn Generator> {
+                Box::new($flag{
+                    width: size.0,
+                    height: size.1,
+                })
+            }
+        }
+
+        pub struct $flag {
+            width: usize,
+            height: usize,
+        }
+
+        impl Generator for $flag {
+            fn get_pixel(&self, pixel: (usize, usize)) -> Color {
+                const STRIPES: &'static [(u32, usize)] = $stripes;
+                FlagStripes::stripes(STRIPES, pixel.1, self.height)
+            }
+        }
+    };
+}
+
+flag_horiz!(EnbyFlagGen, EnbyFlag, &[
+    (0xfcf431ff, 1), (0xfcfcfcff, 1), (0x9d59d2ff, 1), (0x282828ff, 1),
+]);
+
+flag_horiz!(TransFlagGen, TransFlag, &[
+    (0x5bcefaff, 1), (0xf5a9b8ff, 1), (0xffffffff, 1), (0xf5a9b8ff, 1), (0x5bcefaff, 1),
+]);
